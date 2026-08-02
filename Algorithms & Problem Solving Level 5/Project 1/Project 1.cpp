6e35b7c0840ee59c98ff666e144106cd0dbae0be
Mohamed Hamada <mohamedhmadt2@gmail.com>
2026-08-03 02:34:22 +0300

    First Project

---
+++ Algorithms & Problem Solving Level 5/Project 1/Project 1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+    clsDblLinkedList<int> MyDblLinkedList;
+
+    //Insert At Beginning
+    MyDblLinkedList.InsertAtBeginning(5);
+    MyDblLinkedList.InsertAtBeginning(4);
+    MyDblLinkedList.InsertAtBeginning(3);
+    MyDblLinkedList.InsertAtBeginning(2);
+    MyDblLinkedList.InsertAtBeginning(1);
+
+    cout << "Linked List Content:\n";
+    MyDblLinkedList.Print();
+
+    //Find Node
+    clsDblLinkedList<int>::Node* N1 = MyDblLinkedList.Find(2);
+
+    if (N1 != NULL)
+    {
+        cout << "\n\nNode With Value 2 is Found :)\n";
+    }
+    else
+    {
+        cout << "\n\nNode is Not Found :(\n";
+    }
+
+    //Insert After
+    MyDblLinkedList.InsertAfter(N1, 500);
+    cout << "\nAfter Inserting 500 After 2:\n";
+    MyDblLinkedList.Print();
+
+    //Insert At End
+    MyDblLinkedList.InsertAtEnd(700);
+    cout << "\n\nAfter Inserting 700 at End:\n";
+    MyDblLinkedList.Print();
+
+    //Delete Node
+    clsDblLinkedList<int>::Node* N2 = MyDblLinkedList.Find(4);
+    MyDblLinkedList.DeleteNode(N2);
+    cout << "\n\nAfter Delete 4:\n";
+    MyDblLinkedList.Print();
+
+    //Delete First Node
+    MyDblLinkedList.DeleteFirstNode();
+    cout << "\n\nAfter Delete First Node:\n";
+    MyDblLinkedList.Print();
+
+    //Delete Last Node
+    MyDblLinkedList.DeleteLastNode();
+    cout << "\n\nAfter Delete Last Node:\n";
+    MyDblLinkedList.Print();
+
+    system("pause>0");
+    return 0;
+}
---
+++ Algorithms & Problem Solving Level 5/Project 1/clsDblLinkedList.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class clsDblLinkedList
+{
+public:
+
+	class Node
+	{
+	public:
+
+		T value;
+		Node* next;
+		Node* prev;
+	};
+
+	Node* head = NULL;
+
+	void InsertAtBeginning(T value)
+	{
+		Node* new_node = new Node();
+
+		new_node->value = value;
+		new_node->next = head;
+		new_node->prev = NULL;
+
+		if (head != NULL)
+		{
+			head->prev = new_node;
+		}
+		head = new_node;
+	}
+
+	void InsertAfter(Node* current, T value)
+	{
+		if (current == NULL)
+		{
+			return;
+		}
+
+		Node* new_node = new Node();
+
+		new_node->value = value;
+		new_node->next = current->next;
+		new_node->prev = current;
+
+		if (current->next != NULL)
+		{
+			current->next->prev = new_node;
+		}
+		current->next = new_node;
+	}
+
+	void InsertAtEnd(T value)
+	{
+		Node* new_node = new Node();
+
+		new_node->value = value;
+		new_node->next = NULL;
+
+		if (head == NULL)
+		{
+			new_node->prev = NULL;
+			head = new_node;
+			return;
+		}
+
+		Node* current = head;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+		new_node->prev = current;
+	}
+
+	void DeleteNode(Node* NodeToDelete)
+	{
+		if (head == NULL || NodeToDelete == NULL)
+			return;
+
+		if (head == NodeToDelete)
+		{
+			head = NodeToDelete->next;
+		}
+
+		if (NodeToDelete->next != NULL)
+		{
+			NodeToDelete->next->prev = NodeToDelete->prev;
+		}
+
+		if (NodeToDelete->prev != NULL)
+		{
+			NodeToDelete->prev->next = NodeToDelete->next;
+		}
+
+		delete NodeToDelete;
+	}
+
+	void DeleteFirstNode()
+	{
+		if (head == NULL)
+			return;
+
+		Node* FirstNode = head;
+
+		head = head->next;
+
+		if (head != NULL)
+		{
+			head->prev = NULL;
+		}
+		delete FirstNode;
+	}
+
+	void DeleteLastNode()
+	{
+		if (head == NULL)
+			return;
+
+		if (head->next == NULL)
+		{
+			delete head;
+			head = NULL;
+			return;
+		}
+
+		Node* LastNode = head;
+
+		while (LastNode->next != NULL)
+		{
+			LastNode = LastNode->next;
+		}
+
+		LastNode->prev->next = NULL;
+
+		delete LastNode;
+	}
+
+	Node* Find(T value)
+	{
+		Node* current = head;
+
+		while (current != NULL)
+		{
+			if (current->value == value)
+			{
+				return current;
+			}
+			current = current->next;
+		}
+		return NULL;
+	}
+
+	void Print()
+	{
+		Node* current = head;
+
+		while (current != NULL)
+		{
+			cout << current->value << " ";
+			current = current->next;
+		}
+	}
+};
