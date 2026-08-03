2f76e2e10f7205e0a618ab057c0ccd2af52860d1
Mohamed Hamada <mohamedhmadt2@gmail.com>
2026-08-03 22:39:10 +0300

    extension 4

--- Algorithms & Problem Solving Level 5/Project 1/clsDblLinkedList.h
+++ Algorithms & Problem Solving Level 5/Project 1/clsDblLinkedList.h
@@ -170,6 +170,24 @@ public:
 		}
 	}
 
+	void Reverse()
+	{
+		Node* current = head;
+		Node* temp = nullptr;
+
+		while (current != nullptr)
+		{
+			temp = current->prev;
+			current->prev = current->next;
+			current->next = temp;
+			current = current->prev;
+		}
+		if (temp != nullptr)
+		{
+			head = temp->prev;
+		}
+	}
+
 	Node* Find(T value)
 	{
 		Node* current = head;
