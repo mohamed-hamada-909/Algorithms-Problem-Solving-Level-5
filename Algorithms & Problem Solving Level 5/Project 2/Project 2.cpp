9dedf1a0ec1d91581d983c129d4ee0598750e2ea
Mohamed Hamada <mohamedhmadt2@gmail.com>
2026-08-05 03:29:39 +0300

    extension 1 to 7

--- Algorithms & Problem Solving Level 5/Project 2/Project 2.cpp
+++ Algorithms & Problem Solving Level 5/Project 2/Project 2.cpp
@@ -7,11 +7,11 @@ int main()
 {
     clsMyQueue<int> MyQueue;
 
-    MyQueue.push(1);
-    MyQueue.push(2);
-    MyQueue.push(3);
-    MyQueue.push(4);
-    MyQueue.push(5);
+    MyQueue.push(10);
+    MyQueue.push(20);
+    MyQueue.push(30);
+    MyQueue.push(40);
+    MyQueue.push(50);
 
     cout << "Queue:\n";
     MyQueue.print();
@@ -20,6 +20,39 @@ int main()
     cout << "\nQueue Front: " << MyQueue.front();
     cout << "\nQueue Back : " << MyQueue.back();
 
+    //Extension 1
+    cout << "\n\nItem (2): " << MyQueue.GetItem(2);
 
+    //Extension 2
+    MyQueue.Reverse();
+    cout << "\n\nQueue After Reverse: ";
+    MyQueue.print();
+
+    //Extension 3
+    MyQueue.UpdateItem(2, 600);
+    cout << "\n\nMy Queue After Updating Item(2) to 600:\n";
+    MyQueue.print();
+
+    //Extension 4
+    MyQueue.InsertAfter(2, 800);
+    cout << "\n\nMy Queue After Inserting 800 After Item(2): \n";
+    MyQueue.print();
+
+    //Extension 5
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nMy Queue After Inserting 1000 At Front:\n";
+    MyQueue.print();
+
+    //Extension 6
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nMy Queue After Inserting 2000 At Back:\n";
+    MyQueue.print();
+
+    //Extension 7
+    MyQueue.Clear();
+    cout << "\n\nMy Queue After Clear:\n";
+    MyQueue.print();
+
+    system("pause>0");
     return 0;
 }
--- Algorithms & Problem Solving Level 5/Project 2/clsMyQueue.h
+++ Algorithms & Problem Solving Level 5/Project 2/clsMyQueue.h
@@ -47,4 +47,46 @@ public:
 	{
 		return _MyList.GetItem(size() - 1);
 	}
+
+	//Extension 1
+	T GetItem(int index)
+	{
+		return _MyList.GetItem(index);
+	}
+
+	//Extension 2
+	void Reverse()
+	{
+		_MyList.Reverse();
+	}
+
+	//Extension 3
+	void UpdateItem(int index, T value)
+	{
+		_MyList.UpdateItem(index, value);
+	}
+
+	//Extension 4
+	void InsertAfter(int index, T value)
+	{
+		_MyList.InsertAfter(index, value);
+	}
+
+	//Extension 5
+	void InsertAtFront(T value)
+	{
+		_MyList.InsertAtBeginning(value);
+	}
+
+	//Extension 6
+	void InsertAtBack(T value)
+	{
+		_MyList.InsertAtEnd(value);
+	}
+
+	//Extension 7
+	void Clear()
+	{
+		_MyList.Clear();
+	}
 };
